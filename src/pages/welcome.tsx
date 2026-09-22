160c459ed8aafb9adc2008a596556755266aff32
Bijay-Codes <bijayprojects@gmail.com>
2026-09-22 10:49:10 +0530

    fix: wrong cta button being displayed on initial fetch failed

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,18 +1,11 @@
 import { useContext } from "react"
 import { AuthContext, type AuthContextType } from "../context/auth-context"
 import { useNavigate, type NavigateFunction } from "react-router-dom";
-function isRequestSuccess(status: AuthContextType['status'] | undefined) {
-    if (status === null || status === 'pending' || status === undefined) {
-        return false;
-    } else {
-        return true;
-    }
-}
 function CTAButtons({ navTo }: { navTo: NavigateFunction }) {
     const status = useContext(AuthContext)?.status;
     return (
         <section className="flex flex-col gap-4">
-            <span>Our Auto login attempt [ {status === 'pending' ? 'In progress' : status === 'rejected' ? 'got Rejected' : status === 'server-issue' ? 'had some server issues please try again later' : ''} ], please login or register to continue</span>
+            <span className="text-center">Our Auto login attempt [ {status === 'pending' ? 'In progress' : status === 'rejected' ? 'got Rejected' : status === 'server-issue' ? 'had some server issues please try again later' : ''} ], please login or register to continue</span>
             <div className="flex gap-4 m-auto">
                 <button className="bg-fuchsia-400 text-fuchsia-900 py-2 px-6 rounded" onClick={() => navTo('/login')}>Login</button>
                 <button className="bg-violet-400 text-violet-950 px-6 py-2 rounded" onClick={() => navTo('/register')}>
@@ -25,8 +18,12 @@ function CTAButtons({ navTo }: { navTo: NavigateFunction }) {
 export function WelcomeScreen() {
     const navTo = useNavigate();
     const userInfo = useContext(AuthContext);
+    function isRequestSuccess(status: AuthContextType['status'] | undefined) {
+        return status === 'success'
+    }
+
     return (
-        <section className="flex flex-col gap-6 justify-center items-center max-w-200 m-auto text-white h-full mt-auto">
+        <section className="flex flex-col gap-6 justify-center items-center max-w-300 m-auto text-white h-full mt-auto">
             <h1 className="text-2xl font-extrabold">Welcome to Everpet</h1>
             <h2 className="text-center">The backend is starting up please wait (You can expect this to take approx 20-50 seconds depending on your network connection)</h2>
             <h3>Status : {userInfo?.status} </h3>
